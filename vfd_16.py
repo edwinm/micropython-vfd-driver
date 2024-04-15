340ee36f8c9b1fbeb12d283aff1f3aa131d528a8
Edwin Martin <edwin@bitstorm.org>
2024-04-15 13:01:59 +0200

    Add ticker

--- vfd_16.py
+++ vfd_16.py
@@ -1,10 +1,12 @@
 # MicroPython VFD driver for 16 segments
-# Version 1.1.0
-# 2022, Edwin Martin
+# Version 1.2.0
+# 2024, Edwin Martin
+# https://github.com/edwinm/micropython-vfd-driver
 # License: MIT
 
 from micropython import const
 from machine import Pin, SPI
+import time
 
 DIGITS = const(16)
 
@@ -56,16 +58,35 @@ class Display():
 
     def light_off(self):
         self.__command((DISPLAY_LIGHT_OFF, NOT_RELEVANT))
-        
+
     def light_off(self):
         self.__command((DISPLAY_LIGHT_OFF, NOT_RELEVANT))
-        
+
+    def ticker(self, text, fps=5, callback=False):
+        position = 0
+        self.clear()
+
+        while True:
+            if position == len(text):
+                position = 0
+            elif len(text) > 16 and position > len(text) - 16:
+                self.write(text[position:len(text)])
+                self.write(text[0:16 - position], position=len(text) - position)
+
+            if position < len(text):
+                self.write(text[position:position + 16])
+
+            position = position + 1
+            time.sleep_ms(int(1000 / fps))
+            if callback and callback():
+                break
+
     def define_character(self, num, data):
         char = [CGRAM_DATA_WRITE | num]
         lines = data.split('\n')
         if len(lines) > 7:
             lines.remove('')
-        
+
         for col in range(0, 5):
             byte = 0
             bit = 1
@@ -74,7 +95,7 @@ class Display():
                     byte |= bit
                 bit *= 2
             char.append(byte)
-        
+
         self.__command(char)
 
     def __command(self, commands):
@@ -83,3 +104,4 @@ class Display():
         for command in commands:
             self.spi.write(bytearray([command]))
         self.cs(1)
+
